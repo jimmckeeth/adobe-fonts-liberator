b86698a4f7d95b2a1e4ce66a97892bb2945bfd41
Jim McKeeth <jim@mckeeth.org>
2025-04-21 09:56:54 -0600

    clears the hidden attribute
    after copy, the it clears the hidden attribute on the new file

--- adobe-fonts-liberator.ps1
+++ adobe-fonts-liberator.ps1
@@ -49,6 +49,11 @@ Get-ChildItem -Path $AdobeFontsDir -Force | ForEach-Object {
         $fontFile = Join-Path -Path $DestinationDir -ChildPath $uniqueFontName
 
         Copy-Item -Path $_.FullName -Destination $fontFile
+        # Clear hidden attribute if set
+        $fileAttributes = [System.IO.File]::GetAttributes($fontFile)
+        if ($fileAttributes -band [System.IO.FileAttributes]::Hidden) {
+            [System.IO.File]::SetAttributes($fontFile, $fileAttributes -bxor [System.IO.FileAttributes]::Hidden)
+        }
         Write-Output "Liberated`t$_`tto`t$uniqueFontName"
     } catch {
         Write-Error "Failed to process`t$_`tError: $($_.Exception.Message)"
